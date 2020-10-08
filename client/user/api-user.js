96c517c63be7ffc3900dba4c95739c050ceafdfc
MichaelMinkoff <mike.v.minkoff@gmail.com>
2020-10-07 23:41:42 -0700

    user API methods added

---
+++ client/user/api-user.js
@@ -0,0 +1,101 @@
+// api-user stores methods for accessing user CRUD API endpoints
+// allows client to retrieve user data from server
+
+// C- create - sends POST msg for creating a user
+const create = async (user) => {
+  try {
+    let response = await fetch('./api/users/', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(user)
+    })
+    return await response.json()
+  } catch(err) {
+    console.log(err)
+  }
+}
+
+// R - read... or list -- retrieve all users using GET method
+// this will be generic information -- no need for security
+const list = async (signal) => {
+  try {
+    let response = await fetch('/api/users/', {
+      method: 'GET',
+      signal: signal,
+    })
+    return await response.json()
+  } catch(err) {
+    console.log(err)
+  }
+}
+
+// R - read -- retrieve a single user detail using GET method
+// getting specific information -- need credentials in header for security
+// getting user params --- require params as argument + URL
+const read = async (params, credentials, signal) => {
+  try {
+    let response = await fetch('/api/users/' + params.userId, {
+      method: 'GET',
+      signal: signal,
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + credentials.t
+        // NOTE= Bearer scheme for JWT
+      }
+    })
+    return await response.json()
+  } catch(err) {
+    console.log(err)
+  }
+}
+
+
+// U - update -- changing a single user data with PUT method
+// specific user -- need params in URL
+// changing user details -- need security in header
+const read = async (params, credentials, user) => {
+  try {
+    let response = await fetch('/api/users/' + params.userId, {
+      method: 'PUT',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + credentials.t
+        // NOTE= Bearer scheme for JWT
+      },
+      body: JSON.stringify(user)
+    })
+    return await response.json()
+  } catch(err) {
+    console.log(err)
+  }
+}
+
+
+// D - delete -- deleting SPECIFIC user from database - DELETE Method
+// deleting user details -- needs to be protected -- credentials in header
+// need user details -- params in URL
+const remove = async (params, credentials) => {
+  try {
+    let response = await fetch('/api/users/' + params.userId, {
+      method: 'DELETE',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + credentials.t
+        // NOTE= Bearer scheme for JWT
+      },
+    })
+    return await response.json()
+  } catch(err) {
+    console.log(err)
+  }
+}
+
+
+// export CRUD methods
+export { create, list, read, update, remove }
