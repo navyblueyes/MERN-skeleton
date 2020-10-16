d019a685004ab75492ba310cb0a4b591f688f46a
navyblueyes <lock_ed_on@yahoo.com>
2020-10-15 23:42:44 -0700

    added return to Signin

--- client/auth/Signin.js
+++ client/auth/Signin.js
@@ -69,5 +69,24 @@ export default function Signin(props) {
 
   }
 
-
+  return (
+    <Card className={classes.card}>
+      <CardContent>
+        <Typography variant="h6" className={classes.title}>
+          Sign In
+        </Typography>
+        <TextField id="email" type="email" label="Email" className={classes.textField} value={values.email} onChange={handleChange('email')} margin="normal"/><br/>
+        <TextField id="password" type="password" label="Password" className={classes.textField} value={values.password} onChange={handleChange('password')} margin="normal"/>
+        <br/> {
+          values.error && (<Typography component="p" color="error">
+            <Icon color="error" className={classes.error}>error</Icon>
+            {values.error}
+          </Typography>)
+        }
+      </CardContent>
+      <CardActions>
+        <Button color="primary" variant="contained" onClick={clickSubmit} className={classes.submit}>Submit</Button>
+      </CardActions>
+    </Card>
+  )
 }
