4e51742f47d33dd5c9eb80244e87d64702237e18
navyblueyes <lock_ed_on@yahoo.com>
2020-10-17 22:36:25 -0700

    Finished Profile UI

--- client/user/Profile.js
+++ client/user/Profile.js
@@ -1,7 +1,7 @@
 // Profile component [ProCo] shows single user's info
 // user's id is drawn from the url '/user/:userid' path
 // ProCo will conditionally show edit/delete options
-//   ... will provide JWT in the fetch call
+//   ... will retrieve JWT in read call [isAuthenticated method]
 //   ... will be redirected to SignIn if no JWT
 
 export default function Profile({ match }) {
@@ -10,10 +10,11 @@ export default function Profile({ match }) {
 
 
   // useEffect watches for changes to userId; makes call for read to obtain userId
+  // each read returns = ...1) userId ...2) JWT ...3) signal
   useEffect(() => {
     const abortController = new AbortController()
     const signal = abortController.signal
-    const jwt = auth.usAuthenticated()
+    const jwt = auth.isAuthenticated()
     read({
       userId: match.params.userId
     }, {t: jwt.token}, signal).then((data) => {
@@ -28,4 +29,27 @@ export default function Profile({ match }) {
       abortController.abort()
     }
   }, [match.params.userId])
+
+  if (redirectToSignin) {
+    return <Redirect to='/signin'/>
+  }
+
+  return (
+    <Paper className={classes.root} elevation={4}>
+      <Typography variant="h6" className={classes.title}> Profile </Typography> <List dense>
+        <ListItem>
+          <ListItemAvatar>
+            <Avatar>
+              <Person/>
+            </Avatar>
+          </ListItemAvatar>
+          <ListItemText primary={user.name} secondary={user.email}/>
+        </ListItem>
+        <Divider/>
+        <ListItem>
+          <ListItemText primary={"Joined: " + ( new Date(user.created)).toDateString()}/>
+        </ListItem>
+      </List>
+    </Paper>
+  )
 }
