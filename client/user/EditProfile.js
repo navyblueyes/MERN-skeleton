c38b8fe7e1b6ccb0043ca9f850f05018fa93c465
navyblueyes <lock_ed_on@yahoo.com>
2020-10-18 22:30:04 -0700

    finished EditProfile.js UI

--- client/user/EditProfile.js
+++ client/user/EditProfile.js
@@ -78,31 +78,70 @@ export default function EditProfilt({ match }) {
     return function cleanup(){
       abortController.abort()
     }
-
   }, [match.params.userId])
 
   const clickSubmit = () => {
-    const jwt = auth.isAuthenticated()
-      const user = {
-        name: values.name || undefined,
-        email: values.email || undefined,
-        password: values.password || undefined
+    const user = {
+      name: values.name || undefined,
+      email: values.email || undefined,
+      password: values.password || undefined
+    }
+    update({
+      userId: match.params.userId
+    }, {
+      t: jwt.token
+    }, user).then((data) => {
+      if (data && data.error) {
+        setValues({...values, error: data.error})
+      } else {
+        setValues({...values, userId: data._id, redirectToProfile: true})
       }
-      update({
-        userId: match.params.userId
-      }, {
-        t: jwt.token
-      }, user).then((data) => {
-        if (data && data.error) {
-          setValues({...values, error: data.error})
-        } else {
-          setValues({...values, userId: data._id, redirectToProfile: true})
-        }
-      })
+    })
     if (values.redirectToProfile) {
       return (<Redirect to={'/user/' + values.userId} />)
     }
   }
 
+  const handleChange = name => event => {
+    setValues({...values, [name]: event.target.value})
+  }
 
+  if (values.redirectToProfile) {
+    return (<Redirect to={'/user/' + values.userId}/>)
+  }
+
+  return (
+    <Card className={classes.card}>
+      <CardContent>
+        <Typography variant="h6" className={classes.title}>
+          Edit Profile
+        </Typography>
+        <TextField id="name" label="Name" className={classes.textField}
+          value={values.name}
+          onChange={handleChange('name')}
+          margin="normal"/>
+        <br/>
+        <TextField id="email" type="email" label="Email"
+          className={classes.textField}
+          value={values.email}
+          onChange={handleChange('email')}
+          margin="normal"/>
+        <br/>
+        <TextField id="password" type="password" label="Password"
+          className={classes.textField}
+          value={values.password}
+          onChange={handleChange('password')}
+          margin="normal"/>
+        <br/> {
+          values.error && (<Typography component="p" color="error">
+            <Icon color="error" className={classes.error}>error</Icon>
+            {values.error}
+          </Typography>)
+        }
+      </CardContent>
+      <CardActions>
+        <Button color="primary" variant="contained" onClick={clickSubmit} className={classes.submit}>Submit</Button>
+      </CardActions>
+    </Card>
+  )
 }
