67914506a34dbeba001bef6653dac2114dd5d9e3
navyblueyes <lock_ed_on@yahoo.com>
2020-10-20 22:29:38 -0700

    Fixed MainRouter

--- client/MainRouter.js
+++ client/MainRouter.js
@@ -3,18 +3,23 @@ import {Route, Switch} from 'react-router-dom'
 import Home from './core/Home'
 import Users from './user/Users'
 import Signup from './user/Signup'
+import Signin from './user/Signin'
+import EditProfile from './user/EditProfile'
 import Profile from './user/Profile'
+import PrivateRoute from './auth/PrivateRoute'
+import Menu from './core/Menu'
 
 const MainRouter = () => {
   return (
     <div>
+      <Menu />
       <Switch>
         <Route exact path="/" component={Home} />
         <Route path="/users" component={Users} />
+        <Route path="/signup" component={Signup} />
+        <Route path="/signin" component={Signin} />
         <PrivateRoute path="/user/edit/:userId" component={EditProfile} />
         <Route path="/user/:userId" component={Profile} />
-        <Route path="/signup" component={Signup} />
-
       </Switch>
     </div>
   )
