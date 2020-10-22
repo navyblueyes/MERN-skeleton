e59bc74d579c0f632ea51ac9baa04a9b19175bda
navyblueyes <lock_ed_on@yahoo.com>
2020-10-21 20:50:18 -0700

    fixed HTML template for SSR

--- client/main.js
+++ client/main.js
@@ -1,6 +1,12 @@
 import React from 'react'
-import { render } from 'react-dom'
+import { hydrate } from 'react-dom'
 import App from './App'
 
 
-render(<App/>, document.getElementById('root'))
+hydrate(<App/>, document.getElementById('root'))
+
+
+
+// Hydrate allows client to preserver SSR-markup
+// Event Handlers [and some connected CSS] are handled by React
+// Allows for some pre-rendering and bookmarking
