7816d1d052874282caae5ac4a4fb2a9847e7f5cf
navyblueyes <lock_ed_on@yahoo.com>
2020-11-04 21:12:41 -0800

    started on Find API

--- client/user/ProfileTabs.js
+++ client/user/ProfileTabs.js
@@ -14,6 +14,32 @@ import FollowGrid from './../user/FollowGrid'
 import PostList from './../post/PostList'
 
 export default function ProfileTabs ( props ){
+  const [tab, setTab] = useState(0)
+
+  const handleTabChange = (event, value) => {
+    setTab(value)
+  }
+
+  return (
+    <div>
+      <AppBar position="static" color="default">
+        <Tabs
+          value={tab}
+          onChange={handleTabChange}
+          indicatorColor="primary"
+          textColor="primary"
+          variant="fullWidth"
+        >
+          <Tab label="Posts" />
+          <Tab label="Following" />
+          <Tab label="Followers" />
+        </Tabs>
+      </AppBar>
+      {tab === 0 && <TabContainer><PostList removeUpdate={props.removePostUpdate} posts={props.posts}/></TabContainer>}
+      {tab === 1 && <TabContainer><FollowGrid people={props.user.following}/></TabContainer>}
+      {tab === 2 && <TabContainer><FollowGrid people={props.user.followers}/></TabContainer>}
+    </div>
+  )
 }
 
 ProfileTabs.propTypes = {
