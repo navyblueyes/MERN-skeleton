6ebe9063aa31411a5c4b43b14f5a6592b8e854b7
navyblueyes <lock_ed_on@yahoo.com>
2020-11-06 22:33:17 -0800

    working on Post Schema

--- server/models/post.model.js
+++ server/models/post.model.js
@@ -1,4 +1,17 @@
 import mongoose from 'mongoose'
+
+// Focusing on ...
+//  Post -- text [required]
+//          photo [optional]
+//          OP username [required]
+//          Time created
+//          Like --- Array of objects ---
+//              consisting of...
+//                     mongoose ObjectId /
+//          Comment --- Array of object ---
+//              consisting of ...
+//                    text / creations data / OP userId
+
 const PostSchema = new mongoose.Schema({
 
 })
