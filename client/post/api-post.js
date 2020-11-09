ac233e256c091998718611834b8505a9b260fbac
navyblueyes <lock_ed_on@yahoo.com>
2020-11-08 21:35:21 -0800

    started on client post api - listNewsFeed.js

---
+++ client/post/api-post.js
@@ -0,0 +1,18 @@
+// Goal -- Create API for client to connect for retrieving post
+
+const listNewsFeed = async (params, credentials, signal) => {
+  try {
+    let response = await fetch('/api/posts/feed/'+ params.userId, {
+      method: 'GET',
+      signal: signal,
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + credentials.t
+      }
+    })
+    return await response.json()
+  } catch(err) {
+    console.log(err)
+  }
+}
